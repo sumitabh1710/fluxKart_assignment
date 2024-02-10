b547cebd40848229cd9244f4fcf8b2b25e872e0e
Sumitabh1710 <sumitabh1710@gmail.com>
2024-02-10 19:15:24 +0530

    Done with api

--- src/db_setup.js
+++ src/db_setup.js
@@ -1,16 +1,13 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const connectionString =
+  "postgres://sumitabh1710:KUsc5EefPcrLiubMzbBm1qcYPrNC3lIO@dpg-cn3kdr5jm4es73blhbvg-a/fluxcart_contact";
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: connectionString,
 });
 
 async function setupDatabase() {
-
   const enumCheckQuery = `
     SELECT EXISTS (
         SELECT 1
